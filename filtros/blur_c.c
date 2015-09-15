92e62e02f50415f6f754f90308edbe5013e07027
Lucía Rabinowicz <lu.rabinowicz@gmail.com>
2015-09-15 20:29:30 -0300

    arrancando blur

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -3,8 +3,31 @@
 #include <math.h>
 #include "../tp2.h"
 
+ 
+#define e 2.71828182845904523536
+#define M_PI 3.14159265358979323846
 
-void blur_c    (
+
+double funcionGaussiana(float sigma, int radius, int i, int j) {
+
+return (1/(2 * M_PI * sigma * sigma)) * pow(e, ((radius - i) * (radius - i) + (radius - j) * (radius - j)) / (-2 * sigma * sigma));
+}
+
+float* matrizDeConvolucion(float sigma, int radius) {
+float* res = malloc (2 * r + 1) * (2 * r + 1) * 4; //el 4 es porque son floats
+float (*res_matrix)[r * 2 + 1] = (float (*)[r * 2 + 1]) res;
+	for(int i = 0; i < 2 * r + 1; i ++) {
+		for (int j = 0; j < 2 * r + 1; j++) {
+			res[i][j] = funcionGaussiana(sigma, radius, i, j);
+		}
+	}
+return res;
+}
+
+
+
+
+void blur_c (
     unsigned char *src,
     unsigned char *dst,
     int cols,
