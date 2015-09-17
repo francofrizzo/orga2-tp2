57586b9ae06178163e214c8d06c05182d7ba45cd
Manuela Martinez <martinez.manuela.22@gmail.com>
2015-09-17 17:09:40 -0300

    Blur en C compila pero hace cosas raras, hay que revisarlo

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -14,20 +14,20 @@ return (1/(2 * M_PI * sigma * sigma)) * pow(e, ((radius - i) * (radius - i) + (r
 }
 
 float* matrizDeConvolucion(float sigma, int radius) {
-float* res = malloc (2 * r + 1) * (2 * r + 1) * 4; //el 4 es porque son floats
-float (*res_matrix)[r * 2 + 1] = (float (*)[r * 2 + 1]) res;
-	for(int i = 0; i < 2 * r + 1; i ++) {
-		for (int j = 0; j < 2 * r + 1; j++) {
-			res[i][j] = funcionGaussiana(sigma, radius, i, j);
+float* res = malloc((2 * radius + 1) * (2 * radius + 1) * 4); //el 4 es porque son floats - by lu
+float (*res_matrix)[radius * 2 + 1] = (float (*)[radius * 2 + 1]) res;
+	for(int i = 0; i < 2 * radius + 1; i ++) {
+		for (int j = 0; j < 2 * radius + 1; j++) {
+			res_matrix[i][j] = funcionGaussiana(sigma, radius, i, j);
 		}
 	}
 return res;
 }
 
-void afectarPixel(unsigned char *src, unsigned char *dst, float* matConv, int l, int h, int radius){
+void afectarPixel(unsigned char *src, unsigned char *dst, float* matConv, int l, int h, int radius, int cols) {
  unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
  unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
- float (*res_matrix)[r * 2 + 1] = (float (*)[r * 2 + 1]) matConv;
+ float (*matConv_matrix)[radius * 2 + 1] = (float (*)[radius * 2 + 1]) matConv;
 
  double sumaAzul = 0;
  double sumaVerde = 0;
@@ -35,16 +35,16 @@ void afectarPixel(unsigned char *src, unsigned char *dst, float* matConv, int l,
 
     for(int i = 0; i <= 2 * radius; i++){
         for(int j = 0; j <= 2 * radius * 4; j = j + 4){
-           sumaAzul = sumaAzul + src_matrix[l - radius + i][h - radius + j] * matConv[i][j / 4];
-           sumaVerde = sumaVerde + src_matrix[l - radius + i][h - radius + j + 1] * matConv[i][j / 4];
-           sumaRojo = sumaRojo + src_matrix[l - radius + i][h - radius + j + 2] * matConv[i][j / 4];
+           sumaAzul = sumaAzul + src_matrix[l - radius + i][h - radius + j] * matConv_matrix[i][j / 4];
+           sumaVerde = sumaVerde + src_matrix[l - radius + i][h - radius + j + 1] * matConv_matrix[i][j / 4];
+           sumaRojo = sumaRojo + src_matrix[l - radius + i][h - radius + j + 2] * matConv_matrix[i][j / 4];
 
         }
     }
 
 dst_matrix[l][h + 0] = sumaAzul;
-dst_matrix[l][h + 1] =  sumaVerde;
-dst_matrix[l][h + 2] =  sumaRojo;
+dst_matrix[l][h + 1] = sumaVerde;
+dst_matrix[l][h + 2] = sumaRojo;
 dst_matrix[l][h + 3] = 255; 
 
 
@@ -57,11 +57,11 @@ void blur_c(unsigned char *src, unsigned char *dst, int cols, int filas, float s
  unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
     if (radius < filas / 2 && radius < cols / 2) {
-        float* matConv = matrizDeConvolucion(sigma, radius)
-        float (*res_matrix)[r * 2 + 1] = (float (*)[r * 2 + 1]) matConv;
+        float* matConv = matrizDeConvolucion(sigma, radius);
+        float (*res_matrix)[radius * 2 + 1] = (float (*)[radius * 2 + 1]) matConv;
         for(int l = radius; l <= cols - (radius + 1); l ++) {
             for(int h = radius * 4; h <= (filas - (radius + 1)) * 4; h = h + 4) {
-                afectarPixel(src, dst, matConv, l, h, radius);
+                afectarPixel(src, dst, matConv, l, h, radius, cols);
             }
         }
             
