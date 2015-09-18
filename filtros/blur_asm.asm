e82f68232025c647bc65f29d61e431c6e5d89ce7
Lucía Rabinowicz <lu.rabinowicz@gmail.com>
2015-09-17 21:57:09 -0300

    con un toque de assembler en blur. Hice todo en papel y pase una cosita, ahora lo termino en casa

--- filtros/blur_asm.asm
+++ filtros/blur_asm.asm
@@ -8,16 +8,22 @@ section .data
 
 
 section .text
-;void blur_asm    (
-	;unsigned char *src,
-	;unsigned char *dst,
-	;int filas,
-	;int cols,
-    ;float sigma,
-    ;int radius)
+;void blur_asm    (unsigned char *src, unsigned char *dst, int filas, int cols, float sigma, int radius)
 
 _blur_asm:
 blur_asm:
+mov r9, rcx ; r9= filas/2
+div r9, 2
+cmp r9, r8
+jl .seVaDeRangoElRadio ;si radio > filas/2 no se puede
+
+mov r9, rdx ; r9= columnas/2
+div r9, 2
+cmp r9, r8
+jl .seVaDeRangoElRadio ;si radio > filas/2 no se puede
+
+;hasta aca tenemos el primer if
+
 
 
     ret
