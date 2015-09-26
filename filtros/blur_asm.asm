d8ba5cd0d29cd3fcf18e2e3cb180466fac580081
Manuela Martinez <martinez.manuela.22@gmail.com>
2015-09-25 21:16:50 -0300

    cambie un par de cosas de blur_asm

--- filtros/blur_asm.asm
+++ filtros/blur_asm.asm
@@ -17,102 +17,104 @@ _blur_asm:
 
 blur_asm:
 
-push rbp
-mov rbp, rsp
-push rbx
-push r12
-push r13
-push r14
-push r15
-sub rsp, 8
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	sub rsp, 8
 
 
-mov rbx, rdi ;en rbx guardamos la imagen de entrada
-mov r12, rsi ;en r12 guardamos la imagen de salida
-mov r13, rdx ;en r13 guardamos cantidad de filas
-mov r15, rcx ;en r15 guardamos la cantidad de columnas
-;xmm0 esta el sigma
-mov r14, r8 ;en r14 guardamos el radio
+	mov rbx, rdi ;en rbx guardamos la imagen de entrada
+	mov r12, rsi ;en r12 guardamos la imagen de salida
+	mov r13, rdx ;en r13 guardamos cantidad de filas
+	mov r15, rcx ;en r15 guardamos la cantidad de columnas
+	;xmm0 esta el sigma
+	mov r14, r8 ;en r14 guardamos el radio
 
 
-mov r9, r13 ; r9= filas/2
-shr r9, 1
-cmp r9, r14
-jl .seVaDeRangoElRadio ;si radio > filas/2 no se puede
+	mov r9, r13 ; r9= filas/2
+	shr r9, 1
+	cmp r9, r14
+	jl .seVaDeRangoElRadio ;si radio > filas/2 no se puede
 
-mov r9, r15 ; r9= columnas/2
-shr r9, 1
-cmp r9, r14
-jl .seVaDeRangoElRadio ;si radio > columnas/2 no se puede
+	mov r9, r15 ; r9= columnas/2
+	shr r9, 1
+	cmp r9, r14
+	jl .seVaDeRangoElRadio ;si radio > columnas/2 no se puede
 
-;hasta aca tenemos el primer if
+	;hasta aca tenemos el primer if
 
-mov rdi, r14 ;en xmm0 esta el sigma
-call matrizDeConvolucion
-mov r11, rax 
-;r11 esta el puntero a matriz de conv
+	mov rdi, r14 ;en xmm0 esta el sigma
+	call matrizDeConvolucion
+	mov r11, rax 
+	;r11 esta el puntero a matriz de conv
 
 
-;inicializamos l y h para el for
-mov r9, r14 ; r9 = l
-mov r8, r14 ; r8 = h
-shl r8, 2 ; r8 * 4
-
-.ciclo:
-	;cmp r9, (r13 - r14) queremos ver si nos fuimos de las filas
-	mov r10, r13
-	sub r10, r14
-	cmp r9, r10
-	je .yaRecorriLaFoto
-
-	.ciclo2: 
-		;cmp r8, ((r15 - r14)*4) vemos si nos vamos de las columnas
-		mov r10, r15
-		sub r10, r14
-		shl r10, 2 ; r10 * 4
-		cmp r8, r10
-		je .yaRecorriLaFila
-
-		;void afectarPixel(unsigned char *src, unsigned char *dst, float* matConv, int l, int h, int radius, int cols)
-		push r9
-
-		mov rdi, rbx
-		mov rsi, r12
-		mov rdx, r11  
-		mov rcx, r9 
-		;mov r8, r8 
-		mov r9, r14
-		;mov algo, r15
-		push r8
-		push r11
-		push r15
-
-		call afectarPixel 
-		
-		pop r15
-		pop r11
-		pop r8
-		pop r9
-		
-		add r8, 4
-		jmp .ciclo2
-
-.yaRecorriLaFila:
-	mov r8, r14
+	;inicializamos l y h para el for
+	mov r9, r14 ; r9 = l
+	mov r8, r14 ; r8 = h
 	shl r8, 2 ; r8 * 4
-	inc r9
-	jmp .ciclo
 
-.seVaDeRangoElRadio:
-.yaRecorriLaFoto:
-	add rsp, 8
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-	pop rbx
-	pop rbp
-	ret
+	.ciclo:
+		;cmp r9, (r13 - r14) queremos ver si nos fuimos de las filas
+		mov r10, r13
+		sub r10, r14
+		cmp r9, r10
+		je .yaRecorriLaFoto
+
+		.ciclo2: 
+			;cmp r8, ((r15 - r14)*4) vemos si nos vamos de las columnas
+			mov r10, r15
+			sub r10, r14
+			shl r10, 2 ; r10 * 4
+			cmp r8, r10
+			je .yaRecorriLaFila
+		
+			;void afectarPixel(unsigned char *src, unsigned char *dst, float* matConv, int l, int h, int radius, int cols)
+			push r9
+
+			mov rdi, rbx
+			mov rsi, r12
+			mov rdx, r11  
+			mov rcx, r9 
+			;mov r8, r8 
+			mov r9, r14
+			;mov algo, r15
+			push r8
+			push r11
+			push r15
+	
+			call afectarPixel 
+		
+			pop r15
+			pop r11
+			pop r8
+			pop r9
+			
+			add r8, 4
+			jmp .ciclo2
+
+	.yaRecorriLaFila:
+		mov r8, r14
+		shl r8, 2 ; r8 * 4
+		inc r9
+		jmp .ciclo
+
+	.seVaDeRangoElRadio:
+	.yaRecorriLaFoto:
+		add rsp, 8
+		pop r15
+		pop r14
+		pop r13
+		pop r12
+		pop rbx
+		pop rbp
+		ret
+
+
 
 
 afectarPixel_asm:
@@ -134,16 +136,14 @@ push r13
 push r14
 push r15
 
-mov r11, rdx
+mov r15, rdx ;esto lo cambie, antes habia esto "mov r11, rdx". Necesito multiplicar r11, y r15 lo empiezo a usar mas abajo de eso recien. MANU
 
 
 mov rax, r10
-mul rcx
-; en rax tengo (cols * l)
-shl r8, 2
-;en r8 tengo (h*4)
-add r8, rax
-;en r8 tengo (cols * l + h * 4)
+mul rcx ; en rax tengo (cols * l)
+
+shl r8, 2 ;en r8 tengo (h*4)
+add r8, rax ;en r8 tengo (cols * l + h * 4)
 mov r12, rdi
 add r12, r8
 ;mov r12, rdi + cols * l + h * 4; r12 esta apuntando al pixel a aefctar en la matriz entrada
@@ -160,14 +160,19 @@ sub r13, r9
 sub r13, r8
 ;mov r13, r12 - radio - radio * cant columnas ; r13 esta apuntando al primer pixel que tengo que usar para afectarlo
 
-mov rdx, r11
+;mov rdx, r11. Esto lo comente por lo de mas arriba(al final use r15 para guardar rdx) MANU
 
 mov r8, rdx ;guardamos el puntero a la matriz de convolucion 
 
 mov r11, r9 
 shl r11, 1
 inc r11 
+
+mov rax, r11 ;esto lo agregue - MANU
+mul r11 ;esto lo agregue - MANU ; en rax esta r11 * r11 y en rdx tengo la otra parte que no me importa
 ;mul r11, r11		; mov r11, (radio*2 + 1)²
+
+mov rdx, r15 ;esto lo agregue - MANU
 
 mov r15, r9 
 shl r15, 1
@@ -179,7 +184,7 @@ pxor xmm7, xmm7
 .ciclo1:
 	cmp r11, 0 ;r11 es el contador, se fija si ya recorrimos toda la imagen
 	je .yaRecorriLaImagen
-	cmp r15, 4 ;r15 es el contador
+	cmp r15, 4 ;r15 es el contador, se fija si ya recorrimos toda una fila
 	jl .cuantosPixelsQuedan? ;caso borde, me quedan menos que 4 
 
 
@@ -258,7 +263,7 @@ pxor xmm7, xmm7
 	;cmp r8, r13 ;en r8 esta el puntero al primero de la matriz de conv. 
 	;je .esElPrimeroDeMatConv
 
-	sub rdx, 3
+	sub rdx, 12 ;ACA CAMBIE ANTES DECIA "sub rdx, 3" - MANU
 	movdqu xmm1, [rdx]
 	pshufw xmm1, 11111111 ;revisar
 	mulps xmm1, xmm0
@@ -374,7 +379,8 @@ pxor xmm7, xmm7
 
 
 
-;PONER EN 255!!!!!!!!!!!!!!!! Y GUARDAR TODO EL PIXEL 
+;PONER EN 255!!!!!!!!!!!!!!!! Y GUARDAR TODO EL PIXEL
+;en xmm6 tenemos la suma y en r14 esta el puntero al pixel que hay que afectar en la imagen de salida
 ;cuando lo probemos hay que sacar el "extern afectarPixel" 
 
 pop r15
