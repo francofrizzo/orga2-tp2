b2409048a9ab0d33a9d3781fff71d1ee62ea1d8e
Franco Frizzo <francofrizzo@gmail.com>
2015-12-07 18:45:03 -0300

    Cambios en el informe, emprolijé un poquito el código de Blur

--- src/filtros/blur_asm2.asm
+++ src/filtros/blur_asm2.asm
@@ -7,7 +7,7 @@ extern matrizDeConvolucion
 section .data
 
 section .text
-;void blur_asm2 (unsigned char *src, unsigned char *dst, int cols, int filas, float sigma, int radius)
+; void blur_asm2 (unsigned char *src, unsigned char *dst, int cols, int filas, float sigma, int radius)
 
 _blur_asm2:
 blur_asm2:
